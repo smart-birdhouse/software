4454120a4509017bf86b6858aedee16432194f90
tmw0025 <tmw0025@uah.edu>
2020-02-13 18:26:32 -0600

    Fixed script class

--- app/routes.py
+++ app/routes.py
@@ -14,7 +14,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 class scriptThread(object):
     def __init__(self, scriptname, delay=1):
-        thread = threading.Thread(target=self.run, args=scriptname,))
+        thread = threading.Thread(target=self.run, args=(scriptname,))
         thread.daemon = True
         time.sleep(delay)
         thread.start()
