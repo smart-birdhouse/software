5e02016e50272697a82c525fca6ec3d18d92b307
Emma George <srg0010@uah.edu>
2020-02-20 19:40:28 -0600

    extra detection documentation and stopdetection added

--- pir.py
+++ pir.py
@@ -1,6 +1,7 @@
 from RPi import GPIO
 from signal import signal, SIGINT
 from board import D5
+from time import sleep
 
 class PIR:
     """
@@ -10,41 +11,48 @@ class PIR:
     """
 
     def __init__(self):
-        """
-        Starts detection on initialization
-        """
+        """Starts detection on initialization"""
 
-        self._pin = 5 # pin number connected to PIR sensor output wire
+        self._pin = 5  # pin number connected to PIR sensor output wire
 
         # set as input
         GPIO.setup(self._pin, GPIO.IN)
 
-        # set initial value
+        # set initial value, with wait for stabilization
+        sleep(0.5)
         self.movement = GPIO.input(self._pin)
 
         # add a rising edge detection that sets the movement variable to input on the pin
         # callbacks are threaded, meaning this does not interrupt main program execution
         GPIO.add_event_detect(self._pin, GPIO.BOTH,
-                                    callback=self._handler0)
+                              callback=self._callback)
 
-    def _handler0(self):
-        """
-        Updates 'movement' on pin change
-        """
-        self.movement = GPIO.input(self._pin)
+    def _callback(self, pin):
+        """Private callback to update 'movement' on pin change"""
+        self.update(pin)
+
+    def update(self, pin):
+        """Public function to access movement updating for use with custom handler"""
+        self.movement = GPIO.input(pin)
 
     def sleep(self):
-        """
-        Remove detection so the handler is no longer called.
-        """
+        """Remove detection so the handler is no longer called."""
         GPIO.remove_event_detect(self._pin)
 
     def wake(self):
-        """
-        Restart the detection handler
-        """
+        """Restart the detection handler"""
         GPIO.add_event_detect(self._pin, GPIO.BOTH,
-                                    callback=self._handler)
+                              callback=self._callback)
+
+    def newHandler(self, func):
+        """Changes the function called when detecting the pir data event
+
+        Args:
+            func: the function to be used
+        """
+        self.sleep()
+        GPIO.add_event_detect(self._pin, GPIO.BOTH,
+                              callback=func)
 
 
 def handler(signal_received, frame):
@@ -74,3 +82,4 @@ if __name__ == '__main__':
                 print('Motion ended!')
 
         old_value = pir_value
+        sleep(0.2)
